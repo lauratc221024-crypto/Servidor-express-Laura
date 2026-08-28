8b04d512d1a087877266eabe01b2a45f06958469
Laura Tavera <laura.tc221024@gmail.com>
2026-08-28 14:30:00 -0500

    Configurar servidor con CommonJS

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.get("/", (req, res) => {
     res.send("Aprendices ficha 3407186");
 });
 
+app.get("/ruta1", (req, res) => {
+    res.send("Esta es la ruta 1");
+});
+
+app.get("/ruta2", (req, res) => {
+    res.send("Esta es la ruta 2");
+});
+
+app.get("/ruta3", (req, res) => {
+    res.send("Esta es la ruta 3");
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
